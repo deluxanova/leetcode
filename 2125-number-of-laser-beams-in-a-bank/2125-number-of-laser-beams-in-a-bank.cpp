710aa07f1431ab760a67ccf301a4c32f6064836d
Rahul Raj Singh <34077615+deluxanova@users.noreply.github.com>
2022-07-25 12:49:20 +0530

    Time: 196 ms (32.45%), Space: 23.1 MB (73.46%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
@@ -1,30 +1,14 @@
 class Solution {
 public:
     int numberOfBeams(vector<string>& bank) {
-        int n = bank.size(),m = bank[0].size(),cnt = 0;
-        
-        vector<int> levels;
-        for(int i = 0;i<n;i++){
-            cnt = 0;
-            for(auto c:bank[i]){
-                if( c=='1' ) cnt++;
+        int prev=0, curr=0, ans=0;
+        for(auto &a:bank){
+            curr=count(a.begin(),a.end(),'1');
+            if(curr!=0){
+                ans+=prev*curr;
+                prev=curr;
             }
-            levels.push_back(cnt);
         }
-        
-        int ans = 0;
-        for(int i = 0;i<n-1;i++){
-            int curr = levels[i];
-            for(int j = i+1;j<n;j++){
-                if( levels[j]==0 )
-                    continue;
-                else{
-                    ans += curr*levels[j];
-                    break;
-                }
-            }    
-        }
-        return ans;
-        
+       return ans;
     }
 };
