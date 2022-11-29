5f37de7ce08893fbe588c547e67c8bffcb214da7
Rahul Raj Singh <34077615+deluxanova@users.noreply.github.com>
2022-11-29 19:05:58 +0530

    Time: 28 ms (64.15%), Space: 10.7 MB (50.26%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,35 +1,20 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int> mp;
-        vector<int> ans(2,0);
+                unordered_map<int, int> m;
+        vector<int> result;
         
-        for(int i = 0;i<nums.size();i++)
-            mp[ nums[i] ] = i; 
-        
-        for(auto it:mp)
-            cout<<it.first<<" "<<it.second<<endl;
-        cout<<endl;
-
-        
-        
-        for(int i = 0;i<nums.size();i++)
-            nums[i] = target-nums[i];
-        
-        for(auto it:nums)
-            cout<<it<<" ";
-        cout<<endl;
-        
-        
-        for(int i = 0;i<nums.size();i++){
-            //cout<<nums[i];
-            if( mp.find( nums[i] )!=mp.end() && mp[ nums[i] ]!=i ){
-                cout<<nums[i];
-                ans[0] = i;ans[1] = mp[ nums[i] ]; 
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if (m.find(complement) != m.end()) {
+                result.push_back(m[complement]);
+                result.push_back(i);
                 break;
+            } else {
+                m.insert({nums[i], i});
             }
         }
         
-        return ans;
+        return result;
     }
 };
