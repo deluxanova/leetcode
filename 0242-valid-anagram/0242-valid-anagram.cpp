1de5a421b2f9b7b380dbaeafc8556f479b414c4a
Rahul Raj Singh <34077615+deluxanova@users.noreply.github.com>
2023-03-26 17:30:42 +0530

    Time: 15 ms (42.30%), Space: 7.4 MB (61.03%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,21 +1,9 @@
 class Solution {
 public:
-    bool isAnagram(string s1, string s2) {
+    bool isAnagram(string s, string t) {
+        sort( s.begin(),s.end() );
+        sort( t.begin(),t.end() );
         
-        if( s1.size()!=s2.size() )
-            return false;
-        
-        vector<int> occur(27,0);
-        
-        for(int i = 0;i<s1.size();i++){
-            occur[ s1[i]-'a' ]++;
-            occur[ s2[i]-'a' ]--;
-        }  
-        
-        for(int i = 0;i<27;i++){
-            if( occur[i]!=0 )
-                return false;
-        }
-        return true;
+        return s==t;
     }
 };
