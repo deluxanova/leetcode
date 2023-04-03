000a1f91f1403579af21972a36aa29a030fe1c1a
Rahul Raj Singh <34077615+deluxanova@users.noreply.github.com>
2023-04-03 11:52:56 +0530

    Time: 33 ms (21.55%), Space: 25.2 MB (13.43%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,30 +1,45 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int prod = 1;
-        vector<int> cntzro,ans(nums.size(),0);
-        
-        for(int i = 0;i<nums.size();i++){
-            if( nums[i]==0 )
-                cntzro.push_back(i);
+        int zc = 0,zpos = 0,prod = 1,n = nums.size();
+        vector<int> ans(n,0);
+        for(int i = 0;i<n;i++){     
+            if( nums[i]==0 ){
+                zc++;
+                zpos = i;
+            }
+            else
+                prod*=nums[i];
         }
         
-        if(cntzro.size()>1)
+        
+        if( zc>1 )
             return ans;
-        
-        for(auto curr:nums){
-            if(curr!=0)
-                prod *= curr;
-        }
-        
-        if(cntzro.size()==1){
-            ans[ cntzro[0] ] = prod;
+        else if( zc==1 ){
+            ans[zpos] = prod;
             return ans;
         }
         else{
-            for(int i = 0;i<nums.size();i++)
-                ans[i] = prod/nums[i];
+            vector<int> lr(n,0),rl(n,0);
+            
+            prod = 1;
+            for(int i = 0;i<n;i++){
+                prod *= nums[i];
+                lr[i] = prod;
+            }
+             prod = 1;
+            for(int i = n-1;i>=0;i--){
+                prod *= nums[i];
+                rl[i] = prod;
+            }
+            
+
+            int pos = 1;
+            for(int i = 1;i<n-1;i++)
+                ans[i] = lr[i-1]*rl[i+1];
+            ans[0] = rl[1];
+            ans[n-1] = lr[n-2];
         }
-        return ans;   
+        return ans;     
     }
 };
