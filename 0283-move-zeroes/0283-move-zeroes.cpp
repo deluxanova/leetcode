fb3b8a34e1ba1ebdde547b989692fe2a5a02aff8
Rahul Raj Singh <34077615+deluxanova@users.noreply.github.com>
2023-06-29 13:08:54 +0530

    Time: 14 ms (99.04%), Space: 19.2 MB (32.17%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& a) {
+        int n = a.size(),j = -1;
+        //place the pointer j:
+        for (int i = 0; i < n; i++) {
+            if (a[i] == 0) {
+                j = i;
+                break;
+            }
+        }
+    
+        //no non-zero elements:
+        if (j == -1) return;
+    
+        //Move the pointers i and j and swap accordingly:
+        for (int i = j + 1; i < n; i++) {
+            if (a[i] != 0) {
+                swap(a[i], a[j]);
+                j++;
+            }
+        }
+    }
+};
